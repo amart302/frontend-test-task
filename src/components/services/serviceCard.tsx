90c053e7d3a3c150ffb0a6017deebcca7dfcba23
amart302 <amartazanov892@gmail.com>
2025-12-24 17:16:21 +0300

    second commit

--- src/components/services/serviceCard.tsx
+++ src/components/services/serviceCard.tsx
@@ -1,16 +1,18 @@
+import "./services.css";
+import type React from "react";
 import type { Service } from "./services";
 
 interface ServiceCardProps{
     item: Service;
-    add: (id: number) => void;
+    action?: React.ReactNode;
 }
-export default function ServiceCard({ item, add }: ServiceCardProps){
+export default function ServiceCard({ item, action }: ServiceCardProps){
 
     return (
-        <div className="service-card">
+        <div className="service-card" key={ item.id }>
             <p className="service-card__title">{ item.title }</p>
             <div className="service-card__price">{ item.price } ₽</div>
-            <button onClick={ () => add(item.id)}>Добавить</button>
+            { action }
         </div>
     );
 }
--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -1,3 +1,4 @@
+import "./services.css";
 import { useState } from "react";
 import ServiceCard from "./serviceCard";
 
@@ -8,7 +9,7 @@ export interface Service{
 }
 
 export default function Services(){
-    const [ services, setServices ] = useState<Service[]>([
+    const catalogServices = [
         {
             id: 1,
             title: "Услуга А",
@@ -18,21 +19,88 @@ export default function Services(){
             id: 2,
             title: "Услуга Б",
             price: 3000
+        },
+        {
+            id: 3,
+            title: "Услуга В",
+            price: 1500
+        },
+        {
+            id: 4,
+            title: "Услуга Г",
+            price: 4000
+        },
+        {
+            id: 5,
+            title: "Услуга Д",
+            price: 2500
         }
-    ]);
+    ];
 
     const [ selectedServices, setSelectedServices ] = useState<Service[]>([]);
 
-    const add = () => {
+    const addToSelected = (id: number): void => {
+        const foundFromCatalog = catalogServices.find((item: Service) => item.id === id);
+        if(!foundFromCatalog){
+            alert("Услуга не найдена");
+            return;
+        }
 
+        const foundFromSelected = selectedServices.find((item: Service) => item.id === id);
+        if(foundFromSelected){
+            alert("Услуга уже добавлена");
+            return;
+        }
+
+        const updateSelectedServices = [ ...selectedServices, foundFromCatalog ];
+        setSelectedServices(updateSelectedServices);
+    };
+
+    const deleteFromSelected = (id: number): void => {
+        const foundService = catalogServices.find((item: Service) => item.id === id);
+
+        if(!foundService){
+            alert("Услуга не найдена");
+            return;
+        }
+
+        const updateSelectedServices = selectedServices.filter((item: Service) => item.id !== id);
+        setSelectedServices(updateSelectedServices);
+    };
+
+    const finalPrice = (): number => {
+        let totalPrice: number = 0;
+        selectedServices.forEach((item: Service) => totalPrice += item.price);
+        return totalPrice;
+    };
+
+    const placeAnOrder = (): void => {
+        alert("Спасибо! Заказ оформлен");
+        window.location.reload();
     };
 
     return (
-        <div className="service-cards">
+        <div className="main-container">
+            <div className="service-cards">
+                <h2 className="service-cards__title">Каталог услуг</h2>
+                {
+                    catalogServices.map((item: Service) => (
+                        <ServiceCard item={ item } action={ <button className="service-card__action" style={{ backgroundColor: "#80b918" }} onClick={ () => addToSelected(item.id) }>Добавить</button> } />
+                    ))
+                }
+            </div>
             {
-                services.map((item: Service) => (
-                    <ServiceCard item={ item } />
-                ))
+                selectedServices.length > 0 &&
+                <div className="service-cards">
+                    <h2 className="service-cards__title">Корзина</h2>
+                    {
+                        selectedServices.map((item: Service) => (
+                            <ServiceCard item={ item } action={ <button className="service-card__action" style={{ backgroundColor: "#e5383b" }} onClick={ () => deleteFromSelected(item.id) }>Удалить</button> } />
+                        ))
+                    }
+                    <div className="service-cards__total-price">Итого: { finalPrice() }  ₽</div>
+                    <button className="service-cards__order-button" onClick={ () => placeAnOrder()}>Оформить заказ</button>
+                </div>
             }
         </div>
     );
